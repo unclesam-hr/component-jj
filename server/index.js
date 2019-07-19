9b8c6f5ddcc4febc6e50b1746b01996ebeb54f41
jjklee <jjlee720@gmail.com>
2019-07-19 15:29:51 -0700

    [feat] adds gz routing

--- server/index.js
+++ server/index.js
@@ -7,9 +7,22 @@ const { get } = require('./controllers.js');
 const app = express();
 const port = 3003;
 
+app.get('*.js.gz', (req, res, next) => {
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Request-Headers", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/api/product/:id', cors(), get);
