a9ff6823c97d12b6bab9a0fdc36baa6041660688
jjklee <jjlee720@gmail.com>
2019-07-19 16:09:43 -0700

    [ops] updates plugins

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
         test: /\.js[x]?/,
         exclude: /node_modules/,
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ["@babel/plugin-proposal-class-properties"]
         },
       },
     ],
