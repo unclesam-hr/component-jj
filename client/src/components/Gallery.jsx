9359e89553e796258427dd93b16855d6ae3d5844
jjklee <jjlee720@gmail.com>
2019-07-22 19:36:46 -0700

    [refactor] removes unused function

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,35 +1,27 @@
 import React, { Component } from 'react';
 import Swiper from 'react-id-swiper';
 
-export default class Gallery extends Component {
-  handleClick(e, data) {
-    let selectedImage = e.target.src;
-    let el = e.target
-    let imgs = document.getElementsByClassName('carousel-image-selected');
-  }
+export default Gallery = ({ gallery }) => {
+  const params = {
+    noSwiping: true,
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  } 
 
-  render () {
-    const params = {
-      noSwiping: true,
-      slidesPerView: 3,
-      spaceBetween: 10,
-      pagination: {
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }
-    } 
-
-    return this.props.gallery.length  > 0  ? (
-      <Swiper {...params}>
-        {this.props.gallery.map((image, key) => (
-          <div key={key}>
-            <img className="gallery-image" src={image} />
-          </div>
-        ))}
-      </Swiper>
-    ) : null; 
-  }
-};
+  return this.props.gallery.length  > 0  ? (
+    <Swiper {...params}>
+      {gallery.map((image, key) => (
+        <div key={key}>
+          <img className="gallery-image" src={image} />
+        </div>
+      ))}
+    </Swiper>
+  ) : null; 
+}
